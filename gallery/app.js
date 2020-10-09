2c1f7b03f5a2f0c41c1fc6b6d2d88b9f57b55351
io62s <ufo62s@gmail.com>
2020-10-10 00:07:07 +0200

    open close modal

--- gallery/app.js
+++ gallery/app.js
@@ -5,6 +5,41 @@ function Gallery(gallery) {
   const modal = document.querySelector(".modal");
   const prevBtn = modal.querySelector(".prev");
   const nextBtn = modal.querySelector(".next");
+  let currentImage;
+
+  function openModal() {
+    if (modal.matches(".open")) {
+      return;
+    }
+
+    modal.classList.add("open");
+  }
+
+  function closeModal() {
+    modal.classList.remove("open");
+  }
+
+  function handleClickOutside(e) {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  }
+
+  function showImage(el) {
+    if (!el) return;
+
+    modal.querySelector("img").src = el.src;
+    modal.querySelector("h2").textContent = el.title;
+    modal.querySelector("figure p").textContent = el.dataset.description;
+    currentImage = el;
+    openModal();
+  }
+
+  images.forEach((image) => {
+    image.addEventListener("click", (e) => showImage(e.currentTarget));
+  });
+
+  modal.addEventListener("click", handleClickOutside);
 }
 
 const galleryOne = Gallery(document.querySelector(".gallery1"));
